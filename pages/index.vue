24eb9c2d1fa73f1bff7241e72ddd9f19c0d8299f
Timotej Avsec <avsectimotej1@gmail.com>
2021-11-30 22:52:40 +0100

    Add input

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="">
-    <div class="container">
+    <div class="container d-flex flex-column justify-content-start align-items-center p-0 pt-5 mt-5" style="height: 100vh; z-index: 1">
+      
+      <h1 class="logo mt-5">Mubo - URL shortener</h1>
+         <b-input-group class="w-100 ">
+          <b-form-input class="input__field"></b-form-input>
+          <b-input-group-append>
+            <b-button class="action-button">MAKE IT SHORT</b-button>
+          </b-input-group-append>
+        </b-input-group>
     </div>
-    <footer-waves/>
+    <footer-waves style="z-index: -1"/>
   </div>
 </template>
 
@@ -15,13 +23,31 @@ export default {
 <style lang="scss">
 body{
   background-color: #fcd581;
+  padding: 0;
 }
 
 .input__field{
-  padding: 10px;
+  padding: 20px;
   font-size: 1.4rem;
   font-weight: bold;
   border-radius: 10px;
+  background-color: rgba(white, 0.9);
+}
+
+.action-button{
+  background-color: #F25A27;
+  border-color: white;
+
+  &:hover{
+    background-color: #FF7142;
+  }
+}
+
+.logo{
+  font-weight: bolder;
+  margin-bottom: 3rem;
+  font-size: 3rem;
+  text-align: center;
 }
 
 
